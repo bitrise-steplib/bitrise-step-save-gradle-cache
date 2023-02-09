ab15b7ef41f7fea4f82285126a4368f193254903
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-09 11:05:43 +0100

    Use ExitCode constants

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/env"
+	. "github.com/bitrise-io/go-utils/v2/exitcode"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-utils/v2/pathutil"
 	"github.com/bitrise-steplib/steps-save-gradle-cache/step"
 )
 
 func main() {
-	os.Exit(run())
+	exitCode := run()
+	os.Exit(int(exitCode))
 }
 
-func run() int {
+func run() ExitCode {
 	logger := log.NewLogger()
 	envRepo := env.NewRepository()
 	inputParser := stepconf.NewInputParser(envRepo)
@@ -23,13 +25,10 @@ func run() int {
 	pathModifier := pathutil.NewPathModifier()
 	cacheStep := step.New(logger, inputParser, pathChecker, pathProvider, pathModifier, envRepo)
 
-	exitCode := 0
-
 	if err := cacheStep.Run(); err != nil {
 		logger.Errorf(err.Error())
-		exitCode = 1
-		return exitCode
+		return Failure
 	}
 
-	return exitCode
+	return Success
 }
